2a0441018dd07d19619d7cadbadcdb7a974f5cb8
Ateeq Ur Rehman <127838128+Ateeq406721528@users.noreply.github.com>
2023-11-20 14:55:41 +0500

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,40 @@
+package idea_project;
+
+
+
+public class Account {
+    private String accountNumber;
+    private String pin;
+    private double balance;
+
+    public Account(String accountNumber, String pin, double balance) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public boolean validatePin(String enteredPin) {
+        return pin.equals(enteredPin);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+}
+
